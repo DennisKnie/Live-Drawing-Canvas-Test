aa785daf776a8b51dcda9c778a68a9070cfef85a
Dennis Knie <dtknie@live.com>
2017-02-24 10:11:42 -0600

    Updated file
    Changed from drawing dots to smooth lines.

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
 var radius = 10;
+var dragging = false;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var putPoint = function(e) {
-    context.beginPath();
-    context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2);
-    context.fill();
+context.lineWidth = radius*2
+
+var putPoint = function (e) {
+    if(dragging){
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();
+        context.beginPath();
+        context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2);
+        context.fill();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY);
+    }
+};
+
+var engage = function(e) {
+    dragging = true;
+    putPoint(e);
 }
-canvas.addEventListener('mousedown', putPoint);
+
+var disengage = function() {
+    dragging = false;
+    context.beginPath();
+}
+canvas.addEventListener('mouseup', disengage);
+canvas.addEventListener('mousedown', engage);
+canvas.addEventListener('mousemove', putPoint);
