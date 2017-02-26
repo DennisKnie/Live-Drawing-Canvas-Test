617d11d05018862eff70bca9d6b05d6e44f965f2
Dennis Knie <dtknie@live.com>
2017-02-26 12:13:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,29 +7,30 @@ var dragging = false;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.lineWidth = radius*2
+context.lineWidth = radius*2;
 
 var putPoint = function (e) {
     if(dragging){
-        context.lineTo(e.clientX, e.clientY);
-        context.stroke();
-        context.beginPath();
-        context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2);
-        context.fill();
-        context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
+    context.lineTo(e.clientX, e.clientY);
+    context.stroke();
+    context.beginPath();
+    context.arc(e.clientX, e.clientY, radius, 0, Math.PI * 2);
+    context.fill();
+    context.beginPath();
+    context.moveTo(e.clientX, e.clientY);
     }
-};
+}
 
-var engage = function(e) {
+var engage = function(e){
     dragging = true;
     putPoint(e);
 }
 
-var disengage = function() {
+var disengage = function(){
     dragging = false;
     context.beginPath();
 }
-canvas.addEventListener('mouseup', disengage);
+
 canvas.addEventListener('mousedown', engage);
 canvas.addEventListener('mousemove', putPoint);
+canvas.addEventListener('mouseup', disengage);
